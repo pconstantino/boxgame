0404efbec410a01c21760ee58397ad69d2546fee
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-12-18 14:28:51 +0000

    clean the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:flame/flame.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+void main() {}
