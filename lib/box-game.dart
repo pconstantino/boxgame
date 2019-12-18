7f0efea4551273a44774b9bae83e67c63b783442
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-12-18 16:42:24 +0000

    create ui

--- lib/box-game.dart
+++ lib/box-game.dart
@@ -1,14 +1,27 @@
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 
-class BoxGame extends Game
-{
-
+class BoxGame extends Game {
   Size screenSize;
 
   @override
   void render(Canvas canvas) {
-    // TODO: implement render
+    //draw background
+    Rect backgroundRect =
+        Rect.fromLTWH(0.0, 0.0, screenSize.width, screenSize.height);
+    Paint backgroundPaint = Paint();
+    backgroundPaint.color = Colors.black;
+    canvas.drawRect(backgroundRect, backgroundPaint);
+
+    //draw target box
+    double screenCenterX = screenSize.width / 2;
+    double screenCenterY = screenSize.height / 2;
+    Rect boxRect =
+        Rect.fromLTWH(screenCenterX - 75, screenCenterY - 75, 150, 150);
+    Paint boxPaint = Paint();
+    boxPaint.color = Colors.white;
+    canvas.drawRect(boxRect, boxPaint);
   }
 
   @override
@@ -16,10 +29,9 @@ class BoxGame extends Game
     // TODO: implement update
   }
 
-@override
+  @override
   void resize(Size size) {
     screenSize = size;
     super.resize(size);
   }
-
 }
