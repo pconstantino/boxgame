9a086ff3a46527a68c1f5bc291625f3db7312442
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-12-18 14:37:52 +0000

    make the app game

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:boxgame/box-game.dart';
 
-void main() {}
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  BoxGame game = BoxGame();
+  runApp(game.widget);
+}
