d1c4f7e2992782028a68e0883f2a3a7c3fc4ddc6
Pedro Constantino <pedro.constantino@ctw.bmwgroup.com>
2019-12-18 16:30:18 +0000

    add screen size

--- lib/box-game.dart
+++ lib/box-game.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
-
 import 'package:flame/game.dart';
 
 class BoxGame extends Game
 {
+
+  Size screenSize;
+
   @override
   void render(Canvas canvas) {
     // TODO: implement render
@@ -12,6 +14,12 @@ class BoxGame extends Game
   @override
   void update(double t) {
     // TODO: implement update
+  }
+
+@override
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
   }
 
 }
